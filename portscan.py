f1f11476b9375fe95c813731199553a55cf7b35f
Luana FM <37968766+LuanaFM@users.noreply.github.com>
2019-04-22 22:03:15 -0300

    Update portscan.py

--- portscan.py
+++ portscan.py
@@ -1,12 +1,12 @@
 import socket
 
-host = input("Informe o endereco que deseja scannear: ")
-ip = socket.gethostbyname(host)
+host = input("Informe o endereco que deseja scannear: ") 
+ip = socket.gethostbyname(host)                               # Traduz nome para IP
 
 for porta in range (1,65535):
-    scan = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    scan.settimeout(0.10)
+    scan = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # AF_INET = IPV4 e SOCK_STREAM = TCP
+    scan.settimeout(0.10)                                     #Tempo para tentativa de conexão
     result = scan.connect_ex((ip,porta))
-    if result == 0:
+    if result == 0:                                           #Resultado zero significa conexão bem sucedida. 
         print (" Porta: ",str(porta) ,"<---ABERTA")
         scan.close()
